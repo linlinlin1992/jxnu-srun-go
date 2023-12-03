c7b65bbe51b860f6e3effae5484bb1f375d7a76a
linlinlin1992 <linlinlin1992@sina.com>
2023-12-03 16:12:02 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,25 +6,29 @@ path="./out"
 start=$(date +%s)
 
 # Windows
-os="windows"
-arch=("386" "amd64" "arm64")
-for a in ${arch[@]}; do
-    GOOS=$os GOARCH=$a go build -o $path/$name-$os-$a-$version.exe -ldflags -w
-done
+# os="windows"
+# arch=("386" "amd64" "arm64")
+# for a in ${arch[@]}; do
+#     GOOS=$os GOARCH=$a go build -o $path/$name-$os-$a-$version.exe -ldflags -w
+#     echo "Built executable: $path/$name-$os-$a-$version.exe"
+# done
 
 # Linux
 os="linux"
-arch=("386" "amd64" "arm" "arm64" "mips" "mipsle" "mips64" "mips64le" "ppc64" "ppc64le" "s390x" "riscv64")
+# arch=("386" "amd64" "arm" "arm64" "mips" "mipsle" "mips64" "mips64le" "ppc64" "ppc64le" "s390x" "riscv64")
+arch=("amd64")
 for a in ${arch[@]}; do
     GOOS=$os GOARCH=$a go build -o $path/$name-$os-$a-$version -ldflags -w
+    echo "Built executable: $path/$name-$os-$a-$version"
 done
 
 # macOS
-os="darwin"
-arch=("amd64" "arm64")
-for a in ${arch[@]}; do
-    GOOS=$os GOARCH=$a go build -o $path/$name-$os-$a-$version -ldflags -w
-done
+# os="darwin"
+# arch=("amd64" "arm64")
+# for a in ${arch[@]}; do
+#     GOOS=$os GOARCH=$a go build -o $path/$name-$os-$a-$version -ldflags -w
+#     echo "Built executable: $path/$name-$os-$a-$version"
+# done
 
 end=$(date +%s)
 dur=$(($end - $start))
